7239756291d741154bb6987dc50488dbfc3fe75a
alestor123 <alestoraldous@gmail.com>
2021-04-30 13:30:59 +0530

    added mongoose connect and middle

--- App.js
+++ App.js
@@ -20,3 +20,20 @@ cors = require('cors'),
 {join,extname} = require('path'),
 port = process.env.PORT || 3000;
 
+// Mongoose connect
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/Survelliance', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+
+// Multer
+const storages = multer.diskStorage({ destination:process.env.UPLOADS || `./uploads`,filename: (req,file,callback) => callback(null,v4() + extname(file.originalname))})
+const upload = multer({storages});
+
+
+// Static path
+
+app.use('/',express.static(join(__dirname,'serve', 'client')))
+app.use('/dashboard',express.static(join(__dirname,'serve', 'publicc')))
+app.use('/signup',express.static(join(__dirname,'serve', 'signup')))
+app.use('/uploads', express.static(join(__dirname, '/uploads')));
+
+// middleware
